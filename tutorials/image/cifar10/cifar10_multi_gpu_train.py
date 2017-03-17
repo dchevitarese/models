47f2870101d7b9a4fa12c56235f3911a5349388d
Daniel Salles Chevitarese <sallesd@br.ibm.com>
2017-03-17 08:31:51 -0300

    Change where results are saved

--- tutorials/image/cifar10/cifar10_multi_gpu_train.py
+++ tutorials/image/cifar10/cifar10_multi_gpu_train.py
@@ -51,7 +51,7 @@ import cifar10
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
+tf.app.flags.DEFINE_string('train_dir', 'cifar10_train_multi',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 tf.app.flags.DEFINE_integer('max_steps', 1000000,
